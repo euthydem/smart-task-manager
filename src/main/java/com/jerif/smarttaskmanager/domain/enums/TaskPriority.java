1bb60c6d2beb111c9ba88ee33bfc911d60c2bc94
euthydem <jerif174@gmail.com>
2026-04-07 22:26:38 +0400

    feat: add Task entity, enums, and initial Liquibase migration

---
+++ src/main/java/com/jerif/smarttaskmanager/domain/enums/TaskPriority.java
@@ -0,0 +1,7 @@
+package com.jerif.smarttaskmanager.domain.enums;
+
+public enum TaskPriority {
+    LOW,
+    MEDIUM,
+    HIGH
+}
---
+++ src/main/java/com/jerif/smarttaskmanager/domain/enums/TaskStatus.java
@@ -0,0 +1,7 @@
+package com.jerif.smarttaskmanager.domain.enums;
+
+public enum TaskStatus {
+    PENDING,
+    IN_PROGRESS,
+    DONE
+}
